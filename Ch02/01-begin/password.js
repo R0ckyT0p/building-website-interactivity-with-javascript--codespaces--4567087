bf763357a4e0f17d75aeccbadfcbf8884093ffeb
R0ckyT0p <182256394+R0ckyT0p@users.noreply.github.com>
2025-02-26 03:37:55 +0000

    Ch2 Exercises

--- Ch02/01-begin/password.js
+++ Ch02/01-begin/password.js
@@ -1,5 +1,46 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
-    
+    const btnReveal = document.querySelector('#reveal-password');
+    const fldPassword = document.querySelector('#txt-password');
+    const fldPasswordSync = document.querySelector('#txt-password-reveal');
+
+    const fldPasswordInitialDisplay = getComputedStyle(fldPassword).display;
+
+    btnReveal.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      const fldPasswordCurrentDisplay = getComputedStyle(fldPassword).display;
+
+      if (fldPasswordCurrentDisplay !== fldPasswordInitialDisplay) {
+        fldPassword.style.display = fldPasswordInitialDisplay;
+        fldPasswordSync.style.display = "none";
+        btnReveal.innerText = btnReveal.dataset.labelShow;
+      }else {
+        fldPassword.style.display = "none";
+        fldPasswordSync.style.display = fldPasswordInitialDisplay;
+        btnReveal.innerText = btnReveal.dataset.labelHide;
+      }
+      /* This is the First apporach to changing the password field type */
+      // if (fldPassword.type === 'text') {
+      //   fldPassword.type = 'password';
+      //   btnReveal.innerText = btnReveal.dataset.labelShow;
+      // }
+      // else if (fldPassword.type === 'password') {
+      //   fldPassword.type = 'text';
+      //   btnReveal.innerText = btnReveal.dataset.labelHide;
+      // }
+      // else {
+      //   console.error('Unknown password field type');
+      // }
+
+    });
+
+    fldPassword.addEventListener('input', (evt) => {
+      fldPasswordSync.value = fldPassword.value;
+    });
+
+    fldPasswordSync.addEventListener('input', (evt) => {
+      fldPassword.value = fldPasswordSync.value;
+    });
 });
--- Ch02/05-begin/password.js
+++ Ch02/05-begin/password.js
@@ -1,7 +1,30 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
-    // Step 1: Validate that the passwords match when the form is submitted.
-    
-    // Step 2: Wire up toggling password visibility
+  const btnSubmit = document.querySelector('#btn-submit');
+  const btnReveal = document.querySelector('#reveal-pwd');
+  const fldPwd = document.querySelector('#txt-password');
+  const fldPwdConfirm = document.querySelector('#txt-password-confirm');
+
+  // Step 1: Validate that the passwords match when the form is submitted.
+  btnSubmit.addEventListener('click', (evt) => {
+    if (fldPwd.value !== fldPwdConfirm.value) {
+      alert('The passwords do not match.');
+      evt.preventDefault();
+    }
+  });
+
+  // Step 2: Wire up toggling password visibility
+  btnReveal.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (fldPwd.type === 'password') {
+      fldPwd.type = 'text';
+      btnReveal.innerText = btnReveal.dataset.labelHide;
+    } else {
+      fldPwd.type = 'password';
+      btnReveal.innerText = btnReveal.dataset.labelShow;
+    }
+    fldPwdConfirm.type = fldPwd.type;
+  });
+  
 });
